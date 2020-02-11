da7400a6552150e64428049fe20bc89930c3ed52
idris <idris.sabanli@gmail.com>
2020-02-11 11:58:49 +0400

    last st changes

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=40, blank=True, null=True)
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="images", blank=True, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)    
 
     def __str__(self):
--- products_projects/settings.py
+++ products_projects/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'products_projects.urls'
+APPEND_SLASH=False
 
 TEMPLATES = [
     {
@@ -125,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
